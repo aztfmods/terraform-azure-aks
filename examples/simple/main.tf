5c904aa6f43e747c6153a10b436164ab906a0367
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2022-09-05 17:09:11 +0200

    feat: add availability zone support (#6)
    * feat: add availability zone support
    
    * feat: add zones to default_node_pool
    
    * feat: change zones to availability_zones
    
    * feat: bump to latest tf version
    
    * feat: fix fmt issues
    
    * feat: fix zones reference

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -5,7 +5,8 @@ module "aks" {
       config = { location = "westeurope", resourcegroup = "rg-aks-weu" }
       default_node_pool = {
         vmsize = "Standard_DS2_v2"
-        count  = 1
+        zones  = [1, 2, 3]
+        count  = 2
       }
 
       node_pools = {
--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.5.0"
+      source = "hashicorp/azurerm"
     }
   }
 }
