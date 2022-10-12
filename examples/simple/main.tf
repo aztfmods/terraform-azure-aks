39f93bcc31efa2c01bd9d759fcaa8e1e2f24f6a9
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2022-10-12 16:43:47 +0200

    feat: add consistent naming (#10)

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,8 +1,39 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  naming = {
+    company = "cn"
+    env     = "p"
+    region  = "weu"
+  }
+}
+
+module "global" {
+  source = "github.com/aztfmods/module-azurerm-global"
+  rgs = {
+    aks = {
+      name     = "rg-${local.naming.company}-aks-${local.naming.env}-${local.naming.region}"
+      location = "westeurope"
+    }
+  }
+}
+
 module "aks" {
   source = "../../"
+
+  naming = {
+    company = local.naming.company
+    env     = local.naming.env
+    region  = local.naming.region
+  }
+
   aks = {
-    aks1 = {
-      config = { location = "westeurope", resourcegroup = "rg-aks-weu" }
+    demo = {
+      location      = module.global.groups.aks.location
+      resourcegroup = module.global.groups.aks.name
+
       default_node_pool = {
         vmsize = "Standard_DS2_v2"
         zones  = [1, 2, 3]
@@ -15,4 +46,5 @@ module "aks" {
       }
     }
   }
+  depends_on = [module.global]
 }
