9076e604669e8f60c0860f6706323f7b6339bb35
dkool <dennis.kool@cloudnation.nl>
2022-12-13 15:08:25 +0100

    feat: add container_registry integration

---
+++ examples/container-registry/main.tf
@@ -0,0 +1,60 @@
+provider "azurerm" {
+  features {}
+}
+
+module "global" {
+  source = "github.com/aztfmods/module-azurerm-global"
+
+  company = "cn"
+  env     = "p"
+  region  = "weu"
+
+  rgs = {
+    demo = { location = "westeurope" }
+  }
+}
+
+module "acr" {
+  source = "github.com/aztfmods/module-azurerm-acr"
+
+  company = module.global.company
+  env     = module.global.env
+  region  = module.global.region
+
+  registry = {
+    demo = {
+      location      = module.global.groups.demo.location
+      resourcegroup = module.global.groups.demo.name
+      sku           = "Premium"
+    }
+  }
+  depends_on = [module.global]
+}
+
+module "aks" {
+  source = "../../"
+
+  company = module.global.company
+  env     = module.global.env
+  region  = module.global.region
+
+  aks = {
+    demo = {
+      location            = module.global.groups.demo.location
+      resourcegroup       = module.global.groups.demo.name
+      node_resource_group = "${module.global.groups.demo.name}-node"
+      dns_prefix          = "demo"
+
+      registry = {
+        attach = false, role_assignment_scope = module.acr.acr.demo.id
+      }
+
+      default_node_pool = {
+        vmsize     = "Standard_DS2_v2"
+        node_count = 1
+      }
+
+    }
+  }
+  depends_on = [module.global]
+}
