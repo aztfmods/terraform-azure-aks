e717d944d3f9c0e69738d6175c0a187c8aa2498b
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2023-04-07 08:41:43 +0200

    feat: add workload runtime support to default and non default pools (#59)

--- locals.tf
+++ locals.tf
@@ -1,14 +1,15 @@
 locals {
   aks_pools = flatten([
     for pools_key, pools in try(var.aks.node_pools, {}) : {
-      pools_key       = pools_key
-      vmsize          = pools.vmsize
-      count           = pools.node_count
-      max_surge       = pools.max_surge
-      poolname        = "aks${pools_key}"
-      aks_cluster_id  = azurerm_kubernetes_cluster.aks.id
-      linux_os_config = try(pools.config.linux_os, {})
-      kubelet_config  = try(pools.config.kubelet, {})
+      pools_key        = pools_key
+      vmsize           = pools.vmsize
+      count            = pools.node_count
+      max_surge        = pools.max_surge
+      poolname         = "aks${pools_key}"
+      aks_cluster_id   = azurerm_kubernetes_cluster.aks.id
+      linux_os_config  = try(pools.config.linux_os, {})
+      kubelet_config   = try(pools.config.kubelet, {})
+      workload_runtime = try(pools.workload_runtime, null)
     }
   ])
 }
