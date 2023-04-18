b8cf8f4580bd36c319d5ffd4d6f248ebfd5ac478
dkool <dennis.kool@cloudnation.nl>
2023-04-18 17:14:18 +0200

    feat: add windows profile support

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -14,6 +14,35 @@ module "global" {
   }
 }
 
+module "kv" {
+  source = "github.com/aztfmods/module-azurerm-kv"
+
+  company = module.global.company
+  env     = module.global.env
+  region  = module.global.region
+
+  vault = {
+    location      = module.global.groups.demo.location
+    resourcegroup = module.global.groups.demo.name
+
+    secrets = {
+      tls_public_key = {
+        aks = {
+          algorithm = "RSA"
+          rsa_bits  = 2048
+        }
+      }
+    }
+
+    contacts = {
+      admin = {
+        email = "dummy@cloudnation.nl"
+      }
+    }
+  }
+  depends_on = [module.global]
+}
+
 module "aks" {
   source = "../../"
 
@@ -30,6 +59,13 @@ module "aks" {
       vmsize     = "Standard_DS2_v2"
       zones      = [1, 2, 3]
       node_count = 1
+    }
+
+    profile = {
+      linux = {
+        username = "nodeadmin"
+        ssh_key  = module.kv.tls_public_key.aks.value
+      }
     }
   }
   depends_on = [module.global]
